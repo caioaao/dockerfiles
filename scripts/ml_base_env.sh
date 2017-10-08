0029a7c5823e6d63891d974921f1f9a07ac6df7a
caio <caioaao@gmail.com>
2017-10-08 16:48:57 -0300

    base ML images

---
+++ scripts/ml_base_env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ -n "${WITH_GPU:-}" ]]; then
+    DOCKER=nvidia-docker
+    DOCKERIMAGE=caioaao/ml-base-gpu
+else
+    DOCKER=docker
+    DOCKERIMAGE=caioaao/ml-base
+fi
+
+[[ -n "${CONTAINER_NAME:-}" ]] && NAME_PARAM="--name=${CONTAINER_NAME}"
+
+$DOCKER run -it --rm ${NAME_PARAM:-}                    \
+        -v "$(pwd):/project"                            \
+        -v "${DATASETS_DIR:-$(pwd)/datasets}:/datasets" \
+        $DOCKERIMAGE "${@:1}"
