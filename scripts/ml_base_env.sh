f1a5e02b49596fd54353e15f4401ef48ea4cd1c3
caio <caioaao@gmail.com>
2017-10-14 15:29:23 -0300

    correct jupyter port and actually add tensorboard port

--- scripts/ml_base_env.sh
+++ scripts/ml_base_env.sh
@@ -11,10 +11,10 @@ else
 fi
 
 [[ -n "${CONTAINER_NAME:-}" ]] && NAME_PARAM="--name=${CONTAINER_NAME}"
-[[ -z "${TENSORBOARD_PORT:-}" ]] && TENSORBOARD_PORT=8888
 
 $DOCKER run -it --rm ${NAME_PARAM:-}  ${EXTRA_ARGS:-}   \
         -v "$(pwd):/project"                            \
         -v "${DATASETS_DIR:-$(pwd)/datasets}:/datasets" \
-        -p "${TENSORBOARD_PORT}:8888"                   \
+        -p "${JUPYTER_PORT:-8888}:8888"                 \
+        -p "${TENSORBOARD_PORT:-6006}:6006"             \
         $DOCKERIMAGE "${@:1}"
