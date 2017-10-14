fa65c241ec7485a72d7dc07bd3685f68baff8f53
caio <caioaao@gmail.com>
2017-10-14 14:49:36 -0300

    add tensorboard port to ml_base script

--- scripts/ml_base_env.sh
+++ scripts/ml_base_env.sh
@@ -4,15 +4,17 @@ set -euo pipefail
 
 if [[ -n "${WITH_GPU:-}" ]]; then
     DOCKER=nvidia-docker
-    DOCKERIMAGE=caioaao/ml-base-gpu
+    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base-gpu}"
 else
     DOCKER=docker
-    DOCKERIMAGE=caioaao/ml-base
+    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base}"
 fi
 
 [[ -n "${CONTAINER_NAME:-}" ]] && NAME_PARAM="--name=${CONTAINER_NAME}"
+[[ -z "${TENSORBOARD_PORT:-}" ]] && TENSORBOARD_PORT=8888
 
 $DOCKER run -it --rm ${NAME_PARAM:-}  ${EXTRA_ARGS:-}   \
         -v "$(pwd):/project"                            \
         -v "${DATASETS_DIR:-$(pwd)/datasets}:/datasets" \
+        -p "${TENSORBOARD_PORT}:8888"                   \
         $DOCKERIMAGE "${@:1}"
