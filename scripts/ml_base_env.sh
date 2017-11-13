b92f255ec8f134427f1ec244c04489f850152886
caio <caioaao@gmail.com>
2017-11-13 01:11:49 -0200

    Add OpenCV + ML image, bump and improve base

--- scripts/ml_base_env.sh
+++ scripts/ml_base_env.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 if [[ -n "${WITH_GPU:-}" ]]; then
     DOCKER=nvidia-docker
-    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base-gpu}"
+    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base-gpu:2.0.1}"
 else
     DOCKER=docker
-    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base}"
+    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base:1.2.0}"
 fi
 
 [[ -n "${CONTAINER_NAME:-}" ]] && NAME_PARAM="--name=${CONTAINER_NAME}"
