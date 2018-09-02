3d3c419c34dac341b2c84b4b9f8524fc852f68a8
caio <caioaao@gmail.com>
2018-09-02 20:11:16 -0300

    fix rosx11 build script

--- rosx11/build.sh
+++ rosx11/build.sh
@@ -1,4 +1,8 @@
+set -euo pipefail
+
 # Generating docker.xauth (see http://stackoverflow.com/a/25280523)
-xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f docker.xauth nmerge -
+XAUTH_FILE=docker.xauth
+touch $XAUTH_FILE
+xauth nlist :0 | sed -e 's/^..../ffff/' | xauth -f $XAUTH_FILE nmerge -
 
 sudo docker build -t caio.ros:rosx11 .
