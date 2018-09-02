e98658c014ffa0ca3e005995f2f79f3c789db40c
caio <caioaao@gmail.com>
2018-09-02 20:11:09 -0300

    add altair to ml dockerfile

--- scripts/ml_base_env.sh
+++ scripts/ml_base_env.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 
 if [[ -n "${WITH_GPU:-}" ]]; then
     DOCKER=nvidia-docker
-    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base-gpu:4.0.2}"
+    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base-gpu:5.2.0}"
 else
     DOCKER=docker
-    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base:4.0.2}"
+    DOCKERIMAGE="${DOCKERIMAGE:-caioaao/ml-base:5.2.0}"
 fi
 
 [[ -n "${CONTAINER_NAME:-}" ]] && NAME_PARAM="--name=${CONTAINER_NAME}"
